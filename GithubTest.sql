16b06131764c8571b0a3a21c55b452cc243a0a5a
Ashwini <a.patil@cgi.com>
2026-08-10 14:11:24 +0000

    snowflake to git

--- GithubTest.sql
+++ GithubTest.sql
@@ -1,1 +1,7 @@
 select * from AIRBNB.RAW.RAW_HOSTS limit 10 ;
+
+grant usage on warehouse compute_wh to user dbt;
+
+grant ownership on database airbnb to role transform;
+
+grant role transform to user dbt;
